8c6f19a2afd9bf1685c9c289c8513a4b6ec909fa
Unknown <mirirubi6@gmail.com>
2018-02-23 13:52:06 +0100

    esqueleto

---
+++ application.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+});
+
+$('#img3').click(function () {
+    $('#galeria3').hide();
+});
+
+$('#product2').click(function () {
+    $('#galeria3').onShow();
+});
+
+/*$(document).ready(function () {
+    $('button').on('click'.function() {
+        var: precio = $(this).closest('.precio').data('precio');
+    });
+});
+ 
+*/
+
+$("loco").click(function () {
+    $(this).hide();
+});
+
